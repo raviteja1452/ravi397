b910ac2cc7d85a39593f6fb91a895fe3272168e5
raviteja1452 <b.raviteja1452@gmail.com>
2016-07-15 11:12:30 +0530

    Changes to Binary Server

--- HttpServer/BinaryServer.php
+++ HttpServer/BinaryServer.php
@@ -1,120 +1,31 @@
 <?php
 
-require_once('Config.php');
-require_once('HttpServer.php');
-require_once('ServerException.php');
+require_once('BaseServer.php');
 
-class BinaryServer extends HttpServer{
+class BinaryServer extends BaseServer{
 
-	private $address;
-	private $port;
-	private $sock;
-
-	function __construct() {
-		$config = new config();
-		$this->address = $config->address;
-		$this->port = $config->port;
-	}
-
-	function create(){
+	function execute($str){	
 		try{
-				$this->sock = socket_create(AF_INET, SOCK_STREAM, 0);
-				if($this->sock == NULL) throw new ServerException("Could not create socket");
-				$this->bind();
-			}catch(Exception $e){
-					die("Error Message :".$e->getMessage()."\n");
+			if($str['params']['file']){
+				$var = $str['params']['file'];
+				if (file_exists($var)) {
+				    $file = fopen($var,'rb');
+					$output = fread($file,filesize($var));
+					fclose($file);
+				} else {
+				    $output = "FILE NOT FOUND";
+				    echo "FILE NOT FOUND";
+				}
+			}else{
+				$output = "FILE NOT FOUND";
+				echo "FILE NAME NOT FOUND";
 			}
-	}
-
-	function bind(){
-			try{
-				socket_bind($this->sock,$this->address,$this->port);
-				$this->listen();
-			}catch(Exception $e){
-				die("Error Message ".$e->getMessage()."\n");
-			}
-	}
-
-	function listen(){
-			try{
-				socket_listen($this->sock);
-				echo "Started Server and waiting for Clients \n";
-				$this->get();
-			}catch(Exception $e){
-				die("Error Message ".$e->getMessage()."\n");
-			}
-	}
-	function get(){
-		$client  = $this->accept();
-	}
-	function post($arr){
-		$client = $arr['client'];
-		$output = $arr['output'];
-		//echo "To Client :".$output."\n";
-		socket_write($client,$output,strlen($output));
-		socket_close($this->sock);
-	}
-	function fork($client){
-		$pid = pcntl_fork();
-		if($pid == 0)
-		{
-			$arr = $this->parse($client);
-			$this ->post($arr);
-			die("I am Dead Now\n");
-		} else if($pid == -1){
-			die("Could Not fork");
-		} else {	
-			echo "Forked a Client and Waiting for New Client\n";
-		}	
-	}
-	function accept(){
-		while(true)
-		{
-			$client = NULL;
-			try 
-			{
-				$client = socket_accept($this->sock);
-				$this->fork($client);
-				if($client == NULL) die("Socket Accept not working");
-			}catch(Exception $e){
-				die("Error Message".$e->getMessage()."\n");
-			}
-		}
-	}
-
-	function parse($client){	
-		try{
-			$input = NULL;
-			$output = NULL;
-			$input = socket_read($client,1024);
-			$line = explode("\n", $input);
-			$word = explode(" ",$line[0]);
-			$str = explode("=",$word[1]);
-			echo $str[1];
-			if (file_exists($str[1])) {
-			    $file = fopen($str[1],'rb');
-				$output = fread($file,filesize($str[1]));
-				//echo fread($file,filesize($str[1]));
-				fclose($file);
-			} else {
-			    $output = "FILE NOT FOUND";
-			    echo "File not found";
-			}
-			
-			$arr['client'] = $client;
-			$arr['output'] = $output;
-			return $arr;
+			return $output;
 		}catch(Exception $e){
 			die("Error Message ".$e->getMessage()."\n");
 		}
-		socket_close($client);
 	}
-	function __destruct(){
-
-	}
+	
 }
-
-$serv = new BinaryServer();
-$serv->create();
 
 ?>
